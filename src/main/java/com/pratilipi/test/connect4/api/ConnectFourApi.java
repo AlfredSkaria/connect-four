9ede9b18345731a9be91472d37ebe46e347d4a4b
Alfred Skaria <alfiskaria@gmail.com>
2020-09-27 14:02:31 +0530

    Api documentation is done using swagger

--- src/main/java/com/pratilipi/test/connect4/api/ConnectFourApi.java
+++ src/main/java/com/pratilipi/test/connect4/api/ConnectFourApi.java
@@ -8,15 +8,28 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.pratilipi.test.connect4.dto.PlayerMoves;
 
+import io.swagger.annotations.ApiOperation;
+
 public interface ConnectFourApi {
 	
+	@ApiOperation(value = "Get the moves performed by the user", nickname = "getMoves", response = PlayerMoves.class, 
+			tags = {"connect-4" })
 	@GetMapping(value = "/user/moves", produces = "application/json")
 	ResponseEntity<PlayerMoves> getMoves(@RequestParam("playerId") String playerId);
 	
+	@ApiOperation(value = "Insert the coin into a column", nickname = "dropCoin", response = String.class, 
+			tags = {"connect-4" })
 	@PostMapping(value= "/move", produces = "application/json")
 	ResponseEntity<String> dropCoin(@RequestParam("column") String column,@RequestParam("playerId") String playerId) throws JsonProcessingException;
 	
+	@ApiOperation(value = "Start a new game", nickname = "startGame", response = String.class, 
+			tags = {"connect-4" })
 	@PostMapping(value= "/start-game", produces = "application/json")
 	ResponseEntity<String> startGame() throws JsonProcessingException;
+	
+	@ApiOperation(value = "Home controller", nickname = "getHome", response = String.class, 
+			tags = {"connect-4" })
+	@GetMapping("/")
+	ResponseEntity<String> getHome() throws JsonProcessingException;
 
 }
--- src/main/java/com/pratilipi/test/connect4/controller/ConnectFourController.java
+++ src/main/java/com/pratilipi/test/connect4/controller/ConnectFourController.java
@@ -11,11 +11,21 @@ import com.pratilipi.test.connect4.api.ConnectFourApi;
 import com.pratilipi.test.connect4.dto.PlayerMoves;
 import com.pratilipi.test.connect4.service.CoinDropService;
 
+import io.swagger.annotations.Api;
+
 @RestController
+@Api(tags = "connect-4")
 public class ConnectFourController implements ConnectFourApi{
 	
 	@Autowired
 	CoinDropService coinDropService;
+	
+	@Override
+	public ResponseEntity<String> getHome() throws JsonProcessingException {
+		String message = "Please visit swagger for api documentation, https://four-connect-game.herokuapp.com/swagger-ui.html";
+		
+		return ResponseEntity.status(HttpStatus.OK).body(new ObjectMapper().writeValueAsString(message));
+	}
 
 	@Override
 	public ResponseEntity<PlayerMoves> getMoves(String playerId) {
@@ -31,5 +41,7 @@ public class ConnectFourController implements ConnectFourApi{
 	public ResponseEntity<String> startGame() throws JsonProcessingException {
 		return ResponseEntity.status(HttpStatus.OK).body(new ObjectMapper().writeValueAsString(coinDropService.startNewGame()));
 	}
+
+	
 
 }
